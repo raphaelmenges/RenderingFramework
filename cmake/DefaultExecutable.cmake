7dc1334f9b56d02049dd7394028aecd8a9669e87
raphaelmenges <unrealer2@yahoo.de>
2016-03-04 20:58:16 +0100

    Testing OpenGL.

--- cmake/DefaultExecutable.cmake
+++ cmake/DefaultExecutable.cmake
@@ -5,22 +5,16 @@ get_filename_component(PROJECT_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 string(REPLACE " " "_" PROJECT_NAME ${PROJECT_NAME})
 project(${PROJECT_NAME})
 
-# Fetch global code and copy it
-set(CODE ${GLOBAL_CODE})
-
 # Code
 file(GLOB_RECURSE SOURCES *.cpp)
 file(GLOB_RECURSE HEADER *.h)
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-# Combine headers and sources
-set(CODE ${SOURCES} ${HEADERS})
+# Combine headers and sources with global code
+set(CODE ${GLOBAL_CODE} ${SOURCES} ${HEADERS})
 
-# Create executeable
+# Create executable
 add_executable(${PROJECT_NAME} ${CODE})
 
-# Link to executeable
-target_link_libraries(
-	${PROJECT_NAME}
-	glfw
-)
+# Link to executable
+target_link_libraries(${PROJECT_NAME} ${OPENGL_LIBRARIES} glfw)
--- src/exe/ExampleProject/main.cpp
+++ src/exe/ExampleProject/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
+#include "externals/gl3w/include/GL/gl3w.h"
+#include "externals/glfw/include/GLFW/glfw3.h"
+#include "externals/glm/glm/glm.hpp"
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    // Window and OpenGL initialization
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ExampleProject", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    gl3wInit();
+
+    // Prepare delta time calculation
+    float lastTime, deltaTime;
+    lastTime = (float)glfwGetTime();
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Calculate delta time per frame
+        float currentTime = (float)glfwGetTime();
+        deltaTime = currentTime - lastTime;
+        lastTime = currentTime;
+
+        // Test
+        glm::vec4 clearColor(1,1,0,1);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+
+        // Clearing of buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Swap front and back buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Termination of program
+    glfwTerminate();
     return 0;
 }
