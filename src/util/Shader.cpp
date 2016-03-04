21070f64308935a9271e6d7b11ffead71d373da3
raphaelmenges <unrealer2@yahoo.de>
2016-03-04 23:44:33 +0100

    Working on shader.

--- src/util/Shader.cpp
+++ src/util/Shader.cpp
@@ -1,1 +1,26 @@
 #include "Shader.h"
+
+Shader::Shader(std::string vsFilepath, std::string fsFilepath, std::string gsFilepath)
+{
+
+}
+
+Shader::~Shader()
+{
+
+}
+
+void Shader::bind() const
+{
+
+}
+
+void Shader::updateUniform(std::string name, float value) const
+{
+
+}
+
+std::string Shader::readShaderFile(std::string fullpath, std::vector<std::string> defines) const
+{
+    return "";
+}
